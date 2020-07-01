ba10e82dccd6cbde100ecf06a47458e71227c134
Adithya Kamath <adithyakamath@Adithyas-MacBook-Air.local>
2020-07-01 17:39:28 +0530

    Initial commit. Initial Redux store setup

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./src/reducers/PeopleReducer";
+
+import { StyleSheet, Text, View } from "react-native";
+const store = createStore(reducers);
+//given you have installed react native debugger tools -> const store = createStore(reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <View style={styles.container}>
+        <Text>Kams CRM App</Text>
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
---
+++ src/reducers/PeopleReducer.js
@@ -0,0 +1,11 @@
+import people from "./people";
+const initialState = {
+  people
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
